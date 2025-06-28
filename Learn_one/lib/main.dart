cc894eae0f40f92cb0ce67006d6790bb1a3d9c8b
khaichi <adedhani03@gmail.com>
2025-06-28 14:14:22 +0700

    rev 2

--- Learn_one/lib/main.dart
+++ Learn_one/lib/main.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 216, 33, 245),
+                Color.fromARGB(255, 67, 48, 136),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(color: Colors.white, fontSize: 28.0),
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
