94c2cc59d12c809a91d40fb7d1277cd38f06750e
khaichi <adedhani03@gmail.com>
2025-06-29 20:00:41 +0700

    rev 3

--- Learn_one/lib/main.dart
+++ Learn_one/lib/main.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 216, 33, 245),
-                Color.fromARGB(255, 67, 48, 136),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(color: Colors.white, fontSize: 28.0),
-            ),
-          ),
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 216, 33, 245),
+            Color.fromARGB(255, 67, 48, 136),
+          ],
         ),
       ),
-    ),
-  );
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(color: Colors.white, fontSize: 28.0),
+        ),
+      ),
+    );
+  }
 }
