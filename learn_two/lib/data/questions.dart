cfba66d231cb955ced8459205dd15ca5d011cf8a
khaichi <adedhani03@gmail.com>
2025-07-06 09:48:20 +0700

    pertanyaan done

--- learn_two/lib/data/questions.dart
+++ learn_two/lib/data/questions.dart
@@ -8,9 +8,25 @@ const questions = [
     'Carbon',
   ]),
   QuizQuestion('Ada berapa jumlah servo pada robot humanoid?', [
-    'PLA',
-    'TPU',
-    'Karet',
-    'Carbon',
+    '20',
+    '19',
+    '18',
+    '17',
+  ]),
+  QuizQuestion(
+    'Apa aplikasi yang digunakan untuk meelakukan modelling 3D model pada robot?',
+    ['Fusion', 'Coreldraw', 'Affinity Designer', 'Gdocs'],
+  ),
+  QuizQuestion('Berapa torsi yang ada pada servo XM430?', [
+    '4.8 Nm',
+    '4.7 Nm',
+    '4.6 Nm',
+    '4.9 Nm',
+  ]),
+  QuizQuestion('Apa servo yang digunakan pada robot humanoid?', [
+    'XM430',
+    'MX28',
+    'XM540',
+    'MX106',
   ]),
 ];
