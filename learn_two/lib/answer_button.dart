9cbccad78a34a250f98af53541dc92e9fc28bab8
khaichi <adedhani03@gmail.com>
2025-07-07 13:39:34 +0700

    rev 4 (masih di kendala pertanyaan)

---
+++ learn_two/lib/answer_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
+  final String answerText;
+  final void Function() onTap;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        backgroundColor: Color.fromARGB(255, 139, 117, 225),
+        foregroundColor: Color.fromARGB(255, 255, 255, 255),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- learn_two/lib/question_screen.dart
+++ learn_two/lib/question_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:learn_two/data/questions.dart';
+
+import 'answer_button.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,6 +13,22 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text('Question Screen');
+    final currentQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+            child: Text(currentQuestion.text, style: TextStyle(fontSize: 18)),
+          ),
+          SizedBox(height: 30),
+          ...currentQuestion.answer.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
+        ],
+      ),
+    );
   }
 }
