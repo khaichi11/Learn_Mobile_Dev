6cdf7078aa925ceff9d0b2786a0ae2c14ec72d90
khaichi <adedhani03@gmail.com>
2025-07-08 17:29:11 +0700

    rev 4 (masih di kendala pertanyaan)

--- learn_two/lib/question_screen.dart
+++ learn_two/lib/question_screen.dart
@@ -16,18 +16,29 @@ class _QuestionScreenState extends State<QuestionScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-            child: Text(currentQuestion.text, style: TextStyle(fontSize: 18)),
-          ),
-          SizedBox(height: 30),
-          ...currentQuestion.answer.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
-          }),
-        ],
+      child: Container(
+        margin: EdgeInsets.all(50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Text(
+                currentQuestion.text,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 30),
+            ...currentQuestion.answer.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
