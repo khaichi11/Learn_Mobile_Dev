4d0d3c8e3ba765c375b607c1c9f18d6a55d9546f
khaichi <adedhani03@gmail.com>
2025-07-15 22:30:39 +0700

    rev 1 (baru buat text dan dua file tambahan)

--- learn_three/lib/model/expense.dart
+++ learn_three/lib/model/expense.dart
@@ -1,7 +1,19 @@
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
 class Expense {
-  Expense({});
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final String category;
 }
